a0031eb0d67e8ec27c57a0548021aefc0a94b27c
Sheetal Bisht <sheetalbisht41@gmail.com>
2023-09-11 19:34:43 +0530

    trees

--- BinaryTree/src/BinaryTreeArrayRepresentation.java
+++ BinaryTree/src/BinaryTreeArrayRepresentation.java
@@ -3,6 +3,22 @@
 
 //SEQUENTIAL REPRESENTATION (ARRAY)
 //DYNAMIC REPRESENTATION(LINKED LIST)
+
+
+//     CASE 1 (IF INDEX IS START FROM 0)
+//        :- IF A NODE IS AT i INDEX
+//     - LEFT CHILD WOULD BE AT    [(2*i)+1]
+//     - RIGHT CHILD WOULE BE AT   [(2*i)+2]
+//    - PARENT WOULD BE AT         [(i-1)/2]
+//
+//
+//        CASE 2 (IF INDEX IS START FROM 1)
+//        :- IF A NODE IS AT i INDEX
+//        - LEFT CHILD WOULD BE AT    (2*i)
+//        - RIGHT CHILD WOULE BE AT   [(2*i)+1]
+//        - PARENT WOULD BE AT         (i/2)
+
+
 public class BinaryTreeArrayRepresentation {
 
 }
