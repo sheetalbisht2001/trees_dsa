b17bdfeed10793f51cff62711479b6fd776fb08e
Sheetal Bisht <sheetalbisht41@gmail.com>
2023-09-29 02:33:24 +0530

    trees

--- BinaryTree/src/AverageOfLevelInBinaryTree(Leetcode).java
+++ BinaryTree/src/AverageOfLevelInBinaryTree(Leetcode).java
@@ -24,4 +24,58 @@
 
 
 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+          List<Double> arr = new ArrayList<>();
+          Queue<TreeNode> q = new LinkedList<>();
+
+        if (root == null) {
+            return arr;
+        }
+
+        q.add(root);
+
+        while (!q.isEmpty()) {
+            int Size = q.size();
+            double Sum = 0.0;
+
+            for (int i = 0; i < Size; i++) {
+                TreeNode curr = q.poll();
+                Sum = Sum + curr.val;
+
+                if (curr.left != null) {
+                    q.add(curr.left);
+                }
+                if (curr.right != null) {
+                    q.add(curr.right);
+                }
+            }
+
+            double Average = Sum / Size;
+            arr.add(Average);
+        }
+
+        return arr;
+    }
+}
+
+
+
+        
+    
 
