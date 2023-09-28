23f818a70a2bb10cffb08e2c25ec636d8ddc2ccb
Sheetal Bisht <sheetalbisht41@gmail.com>
2023-09-29 01:56:34 +0530

    trees

--- BinaryTree/src/BinaryTreeInorderTraversal(Leetcode).java
+++ BinaryTree/src/BinaryTreeInorderTraversal(Leetcode).java
@@ -25,4 +25,41 @@
 // -100 <= Node.val <= 100
 
 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+
+        ArrayList<Integer> arr = new ArrayList<Integer>() ;
+
+        if(root==null)
+        {
+            return arr ;
+        }
+
+        arr.addAll(inorderTraversal(root.left)) ;
+        arr.add(root.val) ;
+        arr.addAll(inorderTraversal(root.right)) ;
+
+    return arr;
+
+
+
+    }
+
+}
 
--- BinaryTree/src/MaximumDepthOfBinaryTree(Leetcode).java
+++ BinaryTree/src/MaximumDepthOfBinaryTree(Leetcode).java
@@ -21,3 +21,37 @@
 
 // The number of nodes in the tree is in the range [0, 104].
 // -100 <= Node.val <= 100
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root==null)
+        {
+            return 0;
+        }
+
+        int leftHeight = (maxDepth(root.left)) ;
+        int rightHeight = (maxDepth(root.right)) ;
+       int  myHeight = Math.max(leftHeight,rightHeight) + 1 ;
+
+        return myHeight ;
+        
+    }
+}
