808e51ee3b8809b5667c4815f9f553fc22da248a
Sheetal Bisht <sheetalbisht41@gmail.com>
2023-09-28 18:52:25 +0530

    trees

--- BinaryTree/src/EvaluateBooleanBinaryTree(Leetcode).java
+++ BinaryTree/src/EvaluateBooleanBinaryTree(Leetcode).java
@@ -39,6 +39,53 @@
 // Non-leaf nodes have a value of 2 or 3.
 
 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean evaluateTree(TreeNode root) {
+
+       
+
+
+       if(root.val == 1)
+       {
+        return true;
+       }
+      else if(root.val == 0)
+      {
+        return false;
+      }
+      else if(root.val == 2) 
+      {
+        return evaluateTree(root.left) || evaluateTree(root.right);
+
+      }
+      else
+      {
+         return evaluateTree(root.left) && evaluateTree(root.right); 
+
+      }
+
+        
+
+        } 
+    
+}
+
+
 
 
 
