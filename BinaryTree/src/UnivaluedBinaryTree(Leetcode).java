dfa0285df093f77964c0864016ac02bd01c2c8fc
Sheetal Bisht <sheetalbisht41@gmail.com>
2023-09-30 02:09:09 +0530

    trees

--- BinaryTree/src/UnivaluedBinaryTree(Leetcode).java
+++ BinaryTree/src/UnivaluedBinaryTree(Leetcode).java
@@ -22,3 +22,59 @@
 
 // The number of nodes in the tree is in the range [1, 100].
 // 0 <= Node.val < 100
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isUnivalTree(TreeNode root) {
+        ArrayList<Integer> arr = new ArrayList<Integer> () ;
+
+        Stack<TreeNode> stack = new Stack<>() ;
+        stack.push(root) ;
+
+        while(!stack.isEmpty()){
+            TreeNode temp = stack.pop() ;
+            arr.add(temp.val);
+            if(temp.right != null)
+            {
+                stack.push(temp.right) ;
+            }
+            if(temp.left != null)
+            {
+                stack.push(temp.left) ;
+            }
+        }
+
+        int i=0;
+
+      while(i<arr.size())  
+      {
+          if(arr.get(i)!=root.val)
+          {
+              return false ;
+          }
+          else 
+          {
+             i++ ;
+          } 
+      } 
+                return true ;
+    
+    }   
+
+}
