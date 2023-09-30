190805951135c602a18f78f4f855f1a0ae37eb35
Sheetal Bisht <sheetalbisht41@gmail.com>
2023-10-01 01:01:47 +0530

    trees

--- BinaryTree/src/BinaryTreePreorderTraversal(Leetcode).java
+++ BinaryTree/src/BinaryTreePreorderTraversal(Leetcode).java
@@ -23,3 +23,39 @@
 
 // The number of nodes in the tree is in the range [0, 100].
 // -100 <= Node.val <= 100
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        ArrayList<Integer> arr = new ArrayList<Integer>() ;
+
+        if(root==null)
+        {
+            return arr ;
+        }
+
+        arr.add(root.val) ;
+        arr.addAll(preorderTraversal(root.left)) ;
+         arr.addAll(preorderTraversal(root.right)) ;
+
+         return arr ;
+
+        
+    }
+}
