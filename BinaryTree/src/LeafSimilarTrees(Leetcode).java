d0d65eabdc05f63c4eed5b0a3b2404e93c0b7980
Sheetal Bisht <sheetalbisht41@gmail.com>
2023-10-01 01:42:11 +0530

    trees

--- BinaryTree/src/LeafSimilarTrees(Leetcode).java
+++ BinaryTree/src/LeafSimilarTrees(Leetcode).java
@@ -28,3 +28,75 @@
 
 // The number of nodes in each tree will be in the range [1, 200].
 // Both of the given trees will have values in the range [0, 200].
+
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        ArrayList<Integer> arr1 = new ArrayList<Integer>();
+        ArrayList<Integer> arr2 = new ArrayList<Integer>();
+
+        Stack<TreeNode> stack1 = new Stack<>();
+        Stack<TreeNode> stack2 = new Stack<>();
+
+        stack1.push(root1);
+
+        while (!stack1.isEmpty()) {
+            TreeNode temp = stack1.pop();
+            if (temp.left == null && temp.right == null) {
+                arr1.add(temp.val);
+            }
+            if (temp.right != null) {
+                stack1.push(temp.right);
+            }
+            if (temp.left != null) {
+                stack1.push(temp.left);
+            }
+        }
+
+        stack2.push(root2);
+
+        while (!stack2.isEmpty()) {
+            TreeNode temp = stack2.pop();
+            if (temp.left == null && temp.right == null) {
+                arr2.add(temp.val);
+            }
+            if (temp.right != null) {
+                stack2.push(temp.right);
+            }
+            if (temp.left != null) {
+                stack2.push(temp.left);
+            }
+        }
+
+        if (arr1.size() != arr2.size()) {
+            return false;
+        }
+
+        for (int i = 0; i < arr1.size(); i++) {
+            if (!arr1.get(i).equals(arr2.get(i))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
