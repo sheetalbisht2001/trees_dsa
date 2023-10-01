a91f022d0243a7cb2519fff70d2f89b26782839b
Sheetal Bisht <sheetalbisht41@gmail.com>
2023-10-02 00:52:09 +0530

    trees

--- BinaryTree/src/SumOfRootToLeafBinaryNumbers(Leetcode).java
+++ BinaryTree/src/SumOfRootToLeafBinaryNumbers(Leetcode).java
@@ -25,3 +25,26 @@
 
 // The number of nodes in the tree is in the range [1, 1000].
 // Node.val is 0 or 1.
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumRootToLeaf(TreeNode root) {
+        
+    }
+}
