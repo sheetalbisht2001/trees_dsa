c68b068d432450ac82fdb70c440d21ac727d048e
Sheetal Bisht <sheetalbisht41@gmail.com>
2023-10-03 09:33:13 +0530

    trees

--- BinaryTree/src/CountCompleteTreeNodes(Leetcode).java
+++ BinaryTree/src/CountCompleteTreeNodes(Leetcode).java
@@ -28,3 +28,40 @@
 // The number of nodes in the tree is in the range [0, 5 * 104].
 // 0 <= Node.val <= 5 * 104
 // The tree is guaranteed to be complete.
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+
+
+        if(root==null)
+        {
+            return 0 ;
+        }
+
+         if(root.right==null && root.left ==null)
+         {
+             return 1 ;
+         }
+
+         int left = countNodes(root.left) ;
+         int right = countNodes(root.right) ;
+         return 1+ left+right ;
+    }
+}
--- BinaryTree/src/TwoSumIV-InputIsABST(Leetcode).java
+++ BinaryTree/src/TwoSumIV-InputIsABST(Leetcode).java
@@ -22,3 +22,50 @@
 // -104 <= Node.val <= 104
 // root is guaranteed to be a valid binary search tree.
 // -105 <= k <= 105
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+import java.util.ArrayList;
+import java.util.Stack;
+
+class Solution {
+    public boolean findTarget(TreeNode root, int k) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode temp = stack.pop();
+            arr.add(temp.val);
+            if (temp.left != null) {
+                stack.push(temp.left);
+            }
+            if (temp.right != null) {
+                stack.push(temp.right);
+            }
+        }
+
+        for (int i = 0; i < arr.size(); i++) {
+            for (int j = i + 1; j < arr.size(); j++) {
+                if (arr.get(i) + arr.get(j) == k) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
