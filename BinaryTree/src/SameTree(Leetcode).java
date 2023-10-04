3cd4b0ce7c7b4ef0d1a0c351f7d7a60003ba469a
Sheetal Bisht <sheetalbisht41@gmail.com>
2023-10-04 10:03:42 +0530

    trees

--- BinaryTree/src/SameTree(Leetcode).java
+++ BinaryTree/src/SameTree(Leetcode).java
@@ -27,3 +27,36 @@
 
 // The number of nodes in both trees is in the range [0, 100].
 // -104 <= Node.val <= 104
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null && q==null)
+        {
+            return true ;
+        }
+        if(p==null || q==null || p.val!=q.val)
+        {
+            return false ;
+        }
+
+        return isSameTree(p.left,q.left)  && isSameTree(p.right,q.right) ;
+        
+    }
+}
