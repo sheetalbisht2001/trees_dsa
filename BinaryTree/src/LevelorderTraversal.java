ed5085c8cff42e8f71ea3464ca76aca2349c9e10
Sheetal Bisht <sheetalbisht41@gmail.com>
2023-10-06 14:41:49 +0530

    trees

--- BinaryTree/src/LevelorderTraversal.java
+++ BinaryTree/src/LevelorderTraversal.java
@@ -1,4 +1,5 @@
 // Level order traversal
+
 // Given a binary tree, find its level order traversal.
 // Level order traversal of a tree is breadth-first traversal for the tree.
 
@@ -103,37 +104,40 @@ if(node == null) return ans; // at this point ans is also empty;
 
 
 
+
+
+
 // ONE MORE SOLUTION TO THIS QUESTION
 
 
 
-//     class Solution
-// {
-//     //Function to return the level order traversal of a tree.
-//     static ArrayList <Integer> levelOrder(Node node) 
-//     {
-//         // Your code here
-//         int root_value = node.data;
-//         ArrayList<Integer> arr = new ArrayList();
-//         ArrayList<Node> temp_arr = new ArrayList();
-//         arr.add(root_value);
-//         temp_arr.add(node);
+    class Solution
+{
+    //Function to return the level order traversal of a tree.
+    static ArrayList <Integer> levelOrder(Node node) 
+    {
+        // Your code here
+        int root_value = node.data;
+        ArrayList<Integer> arr = new ArrayList();
+        ArrayList<Node> temp_arr = new ArrayList();
+        arr.add(root_value);
+        temp_arr.add(node);
         
-//         while(!temp_arr.isEmpty())
-//         {
-//             Node fv = temp_arr.get(0);
-//             Node fvr = fv.right;
-//             Node fvl = fv.left;
-//             if(fvl != null) {arr.add(fvl.data); temp_arr.add(fvl);}
-//             if(fvr != null) {arr.add(fvr.data); temp_arr.add(fvr);}
+        while(!temp_arr.isEmpty())
+        {
+            Node fv = temp_arr.get(0);
+            Node fvr = fv.right;
+            Node fvl = fv.left;
+            if(fvl != null) {arr.add(fvl.data); temp_arr.add(fvl);}
+            if(fvr != null) {arr.add(fvr.data); temp_arr.add(fvr);}
             
-//             temp_arr.remove(0);
-//         }
+            temp_arr.remove(0);
+        }
         
-//         return arr;
+        return arr;
         
-//     }
-// }
+    }
+}
 
 
 
